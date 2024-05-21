7bd7d557fd92667db457e3e60ed8ceba990cecee
Sokphal-Pon <cpc205020@tnut.edu.vn.com>
2024-05-21 21:06:22 +0700

    commit

--- facial_decode.py
+++ facial_decode.py
@@ -1,1 +1,46 @@
-print('hello')
+import cv2
+import face_recognition
+import pickle
+import os
+
+
+# Importing student images
+folderPath = 'students'
+pathList = os.listdir(folderPath)
+print(pathList)
+imgList = []
+studentID = []
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentID.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+    #print(path)
+    #print(os.path.splitext(path)[0])
+print(studentID)
+
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentID]
+print("Encoding Complete")
+
+file = open("trainFiles.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved")
