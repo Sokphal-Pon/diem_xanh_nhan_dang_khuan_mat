19e02398dfd516157f34562f892c6c195b803cf7
Sokphal-Pon <cpc205020@tnut.edu.vn.com>
2024-05-27 20:59:20 +0700

    up commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-test = 'hello world'
-print(test)
+import pyodbc
+#import numpy as np
+
+# Connect to SQL Server
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=YOURS\HELLOSQL;DATABASE=SDdatabase;UID=sa;PWD=hello@123')
+cursor = conn.cursor()
+
+# Execute SQL query
+cursor.execute("SELECT * FROM sinh_vien_info")
+
+# Fetch data
+students = cursor.fetchall()
+
+for student in students:
+    print(student)
+
+# Close connection
+conn.close()
